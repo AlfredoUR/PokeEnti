bbf95716c73de12d8372112304a82f1144eee00d
Pol Blesa <pol.blesa@enti.cat>
2024-05-06 18:59:16 +0200

    header

--- PokeEnti/PokeEnti.cpp
+++ PokeEnti/PokeEnti.cpp
@@ -1,42 +1,4 @@
-#include <iostream>
-#include <fstream>
-#include <windows.h>
-#include <vector>
-#include <cstdlib>
-#include <ctime>
-
-using namespace std;
-
-// Estructuras y Funciones
-
-struct Map {
-    char** data;
-    int width;
-    int height;
-};
-
-struct Configuration {
-    int mapWidth;
-    int mapHeight;
-    std::vector<std::pair<int, int>> pokemonCounts;
-};
-
-struct Player {
-    int x;
-    int y;
-    char direction;
-    int pokemonCaptured;
-};
-
-// Declaraciones de funciones
-Configuration loadConfiguration(const char* filename);
-Map createMap(int width, int height);
-void displayMap(Map& map, Player& player);
-void movePlayer(Player& player, int dx, int dy, Map& map);
-void capturePokemon(Player& player, Map& map);
-void unlockZones(Player& player, Map& map, const Configuration& config);
-void gameLoop(Map& map, Player& player, const Configuration& config);
-void freeMap(Map& map);
+#include "PokeEnti.h"
 
 // Definiciones de funciones
 
