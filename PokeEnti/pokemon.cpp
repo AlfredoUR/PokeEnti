79db0e1d95cb669be9630610dd63407bdfd0757d
Pol Blesa <pol.blesa@enti.cat>
2024-06-18 01:28:05 +0200

    pokemons

---
+++ PokeEnti/pokemon.cpp
@@ -0,0 +1,26 @@
+#include "pokemon.h"
+
+Pokemons::Pokemons(int posX, int posY, char simbolo, int vida) : posX(posX), posY(posY), simbolo(simbolo), vida(vida) {}
+
+int Pokemons::ObtenerPosX() const {
+    return posX;
+}
+
+int Pokemons::ObtenerPosY() const {
+    return posY;
+}
+
+char Pokemons::ObtenerSimbolo() const {
+    return simbolo;
+}
+
+int Pokemons::ObtenerVida() const {
+    return vida;
+}
+
+void Pokemons::DisminucionVida(int cantidad) {
+    vida -= cantidad;
+    if (vida < 0) {
+        vida = 0;
+    }
+}
--- PokeEnti/pokemon.h
+++ PokeEnti/pokemon.h
@@ -1,1 +1,21 @@
+
 #pragma once
+
+#include "liderGimnasio.h"
+
+
+class Pokemons {
+public:
+	Pokemons(int posX, int posY, char simbolo, int vida);
+	int ObtenerPosX() const;
+	int ObtenerPosY() const;
+	char ObtenerSimbolo() const;
+	int ObtenerVida() const;
+	void DisminucionVida(int cantidad);
+
+private:
+	int posX;
+	int posY;
+	char simbolo;
+	int vida;
+};
