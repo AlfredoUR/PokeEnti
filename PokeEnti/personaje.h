c54437774fd506a598aa3102fd8a68995ad3f492
Pol Blesa <pol.blesa@enti.cat>
2024-06-18 20:26:17 +0200

    personaje.h

--- PokeEnti/personaje.h
+++ PokeEnti/personaje.h
@@ -1,1 +1,40 @@
+
 #pragma once
+
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <algorithm>
+#include <chrono>
+#include <thread>
+#include <vector>
+#include <windows.h>
+#include <numeric>
+#include <cstdlib>
+#include <ctime>
+#include <limits>
+
+class Personaje {
+public:
+    Personaje(int posX, int posY, char simbolo, int pokemonDanyo);
+    int ObtenerPosX() const;
+    int ObtenerPosY() const;
+    char ObtenerSimbolo() const;
+    void Movimiento(int x, int y);
+    void Posicion(int x, int y);
+    void ActualizarSimbolo(int x, int y);
+    int ObtenerPokeballsRecogidas() const;
+    void PokeballsRecogidas();
+    int ObtenerPokemonsCapturados() const;
+    void AumentarCuentaPokemons();
+    void Reiniciar() { pokeballs = 0; pokemonsCapturados = 0; }
+    int ObtenerPokemonDanyo() const;
+private:
+    int posX;
+    int posY;
+    char simboloPersonaje;
+    int pokeballs = 0;
+    int pokemonsCapturados = 0;
+    int pokemonDanyo;
+};
